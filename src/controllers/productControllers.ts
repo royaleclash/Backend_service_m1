7c3ee507d90e0638ac163fce905f308c6d6d74b3
fico16 <fedepineda09@gmail.com>
2024-11-01 08:36:03 -0500

    actualiacionn controlador

--- src/controllers/productControllers.ts
+++ src/controllers/productControllers.ts
@@ -39,6 +39,7 @@ export const getProductByID = async (req: Request, res: Response) => {
         }
     } catch (error) {
         res.status(500).json({
+            message:"error al obtener el producto"
 
 
         })
@@ -46,11 +47,27 @@ export const getProductByID = async (req: Request, res: Response) => {
     };
 }
 
-//crear un productos
+//crear un productos (POST)
 export const createproduct = async (req: Request, res: Response) => {
     try {
+        const {name, description, price} = req.body;
+        const product = new Product ();
+        product.name = name;
+        product.description = description;
+        product.price = price;
+        await productRepository.save(product);
+        res.status(201).json(product);
 
     } catch (error) {
+        res.status(500).json({
+            message:"error al crear un producto"
+
+
+        })
+
+        
+
+
 
     }
 
@@ -59,8 +76,36 @@ export const createproduct = async (req: Request, res: Response) => {
 //Actulizar un producto
 export const updateproduct = async (req: Request, res: Response) => {
     try {
+        const {name, description, price} = req.body;
+        const product = await productRepository.findOneBy ({
+            id : parseInt(req.params.id)
+        })
+        
+        //validamos que product tenga informacion
+
+        if (product) {
+            product.name = name ?? product.name;
+            product.description = description ?? product.description
+            product.price = price ?? product.price;
+            await productRepository.save(product)
+            res.json(product);
+            
+        } else {
+            res.status(404).json({
+                message: "no se encontro el producto."
+            })
+
+        } 
+
 
     } catch (error) {
+        
+    res.status(500).json({
+         message:"error al actualizar un producto"
+    
+    
+            })
+    
 
     }
 
@@ -69,8 +114,35 @@ export const updateproduct = async (req: Request, res: Response) => {
 //Eliminar un producto existente
 export const deleteproduct = async (req: Request, res: Response) => {
     try {
+        const product = await productRepository.findOneBy ({
+            id : parseInt(req.params.id)
+        })
+        
+        //validamos que product tenga informacion
+
+        if (product) {
+           
+          await productRepository.remove(product)
+            res.json({
+                message: "Producto eliminado."
+            })
+            
+        } else {
+            res.status(404).json({
+                message: "no se encontro el producto."
+            })
+
+        } 
+
 
     } catch (error) {
+        
+    res.status(500).json({
+        message:"error al eliminar un producto"
+    
+    
+            })
+     
 
     }
 
